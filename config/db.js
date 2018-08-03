f76d8419c71498d6d210f6f0931e441ac883e412
Azhor Ahai <debapriya.das.blr@gmail.com>
2018-08-03 17:19:27 +0530

    remove console.logs

--- config/db.js
+++ config/db.js
@@ -4,14 +4,12 @@
 const mongodb = require('mongodb')
 , config = require('./index');
 
-console.log(config);
 var Server = mongodb.Server
 , MongoClient = mongodb.MongoClient;
 
 
 //console.log(Server, MongoClient);
 module.exports.init = function (callback) {
-  console.log(config);
   let mongoClient = new MongoClient(new Server(config.databaseHost, config.databasePort));
   mongoClient.connect( (err, mongoClient)=> {
     mongoClient.db(config.databaseName);
