f85ab33c3efa986e9abc658540da81f849dfe9d0
Debapriya Das <debapriya.das@efi.com>
2018-11-06 17:48:56 +0530

    add everything changed before going

---
+++ app/models/index.js
@@ -0,0 +1,36 @@
+'use strict';
+const fs        = require('fs');
+const path      = require('path');
+const Sequelize = require('sequelize');
+const config    = require(__dirname + '/../../config');
+const appDir    = config.appDir;
+const basename  = path.basename(__filename);
+const db        = {};
+const DBCONFIG = require(appDir + '/config/mysqlconfig');
+
+const sequelize = new Sequelize(DBCONFIG.db_name, DBCONFIG.db_user, DBCONFIG.db_password, {
+  host: DBCONFIG.db_host,
+  dialect: DBCONFIG.db_dialect,
+  port: DBCONFIG.db_port,
+  operatorsAliases: false
+});
+
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach((file) => {
+    let model = sequelize['import'](path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
